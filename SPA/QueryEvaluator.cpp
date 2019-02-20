cff0da058e5172609022149a47921207d20c1fd8
HyperionNKJ <kjneo1996@gmail.com>
2019-02-20 18:24:14 +0800

    Refactor QueryResult to ResultProjector

--- SPA/QueryEvaluator.cpp
+++ SPA/QueryEvaluator.cpp
@@ -1,18 +1,18 @@
 #include "QueryEvaluator.h"
-#include "QueryResult.h"
+#include "ResultProjector.h"
 #include "Clause.h"
 #include <unordered_set>
 
 std::list<std::string> QueryEvaluator::evaluate(ProcessedQuery processedQuery, PKB pkb) {
-	QueryResult queryResult; // QueryProjector
-	queryResult.resetResults(); // Reset possible old query result
+	ResultProjector resultProjector;
+	resultProjector.resetResults(); // Reset possible old query result
 	std::list<std::string> finalResult; // initially empty
 
 	std::unordered_set<Clause> clauses = processedQuery.getClauses();
 	for (auto clause : clauses) {
 		Result clauseResult = clause.evaluate(pkb);
 		if (clauseResult.hasPassed() && clauseResult.hasAnswer()) { // hasPassed && noAnswer denotes a True/False clause
-			bool hasResultSoFar = queryResult.combineResults(clauseResult.getAnswer());
+			bool hasResultSoFar = resultProjector.combineResults(clauseResult.getAnswer());
 			if (!hasResultSoFar) {
 				return finalResult; // return empty result
 			}
@@ -21,5 +21,5 @@ std::list<std::string> QueryEvaluator::evaluate(ProcessedQuery processedQuery, P
 			return finalResult;
 		}
 	}
-	return queryResult.getResults(processedQuery.getSelectedSynonym()); // To be clarified.
+	return resultProjector.getResults(processedQuery.getSelectedSynonym()); // To be clarified.
 }
