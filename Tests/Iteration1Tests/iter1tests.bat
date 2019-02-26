878cf1e902fea46c4db219d7474f5bc6964d6a1f
unknown <leook0209@gmail.com>
2019-02-26 15:26:06 +0800

    Make declare statement in one line

--- Tests/Iteration1Tests/iter1tests.bat
+++ Tests/Iteration1Tests/iter1tests.bat
@@ -1,6 +1,4 @@
 for /d %%f in (*) do (
 for /d %%g in (%%f\*) do (
 for /r %%i in (%%g\*.txt) do (
-start /w /b ..\..\Debug\AutoTester.exe %%f\source.txt %%i %%~pi%%~ni.xml
-
- )))
+start /w /b ..\..\Debug\AutoTester.exe %%f\source.txt %%i %%~pi%%~ni.xml )))
