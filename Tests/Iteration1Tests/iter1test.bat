ff60dc52a8543d08f52b91fa6bee1c897582b2ba
unknown <leook0209@gmail.com>
2019-02-26 11:41:04 +0800

    Edit iteration1 test script to pause

--- Tests/Iteration1Tests/iter1test.bat
+++ Tests/Iteration1Tests/iter1test.bat
@@ -1,4 +1,5 @@
 for /d %%f in (*) do (
 for /d %%g in (%%f\*) do (
 for /r %%i in (%%g\*) do (
-..\..\Debug\AutoTester.exe %%f\source.txt %%i %%i.xml )))
+..\..\Debug\AutoTester.exe %%f\source.txt %%i %%i.xml 
+pause )))
