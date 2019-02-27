4cf2ee77fdcab46fdc6a435904fd8440ec690daa
unknown <leook0209@gmail.com>
2019-02-27 15:45:44 +0800

    Add Parser To PKB integration test

--- IntegrationTesting/TestFrontEnd.cpp
+++ IntegrationTesting/TestFrontEnd.cpp
@@ -1,18 +1,37 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
+#include "Parser.h"
+#include "PKB.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace IntegrationTesting
 {		
-	TEST_CLASS(UnitTest1)
+	TEST_CLASS(TestParserToPKB)
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(TestInsertProcedure)
 		{
-			// TODO: Your test code here
-		}
+			Parser parser;
+			PKB pkb;
+			parser.setPKB(&pkb);
+			unordered_set<string> procs;
 
+			procs = pkb.getAllProcedures();
+			Assert::AreEqual((int)procs.size(), 0);
+
+			string procName = "myProcOne";
+			parser.handleProcedure("procedure myProcOne {");
+
+			procs = pkb.getAllProcedures();
+			Assert::AreEqual((int) procs.size(), 1);
+
+			string wrongName = "hahahaha";
+			for (string it : procs) {
+				Assert::AreEqual(it, procName);
+				Assert::AreNotEqual(it, wrongName);
+			}
+		}
 	};
 }
