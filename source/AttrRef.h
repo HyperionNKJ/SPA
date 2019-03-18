2b4a8e0f4198dd1cb62c7c260ebf298002139820
alexkmj <alex@op.sg>
2019-03-18 15:41:41 +0800

    Added AttrRef and modified DesignEntity to contain AttrRef

---
+++ source/AttrRef.h
@@ -0,0 +1,5 @@
+#pragma once
+
+enum AttrRef {
+	PROC_NAME, VAR_NAME, STMT_NUM, VALUE, UNASSIGNED
+};
--- source/DesignEntity.cpp
+++ source/DesignEntity.cpp
@@ -5,6 +5,13 @@ DesignEntity::DesignEntity() {}
 DesignEntity::DesignEntity(const string& value, const Type& type) {
 	this->value = value;
 	this->type = type;
+	this->attrRef = AttrRef::UNASSIGNED;
+}
+
+DesignEntity::DesignEntity(const string& value, const Type& type, const AttrRef& attrRef) {
+	this->value = value;
+	this->type = type;
+	this->attrRef = attrRef;
 }
 
 void DesignEntity::setValue(const string& value) {
@@ -15,10 +22,18 @@ void DesignEntity::setType(const Type& type) {
 	this->type = type;
 }
 
+void DesignEntity::setAttrRef(const AttrRef& attrRef) {
+	this->attrRef = attrRef;
+}
+
 string DesignEntity::getValue() {
 	return this->value;
 }
 
 Type DesignEntity::getType() {
 	return this->type;
+}
+
+AttrRef DesignEntity::getAttrRef() {
+	return this->attrRef;
 }
