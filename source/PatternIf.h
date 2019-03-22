e8935a55af68bdb0277891e21956262a776a29c5
HyperionNKJ <kjneo1996@gmail.com>
2019-03-22 23:05:39 +0800

    Add evaluation for If Pattern

---
+++ source/PatternIf.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Clause.h"
+
+class PatternIf : public Clause {
+private:
+	DesignEntity subject; // subject can only be of IF synonym
+
+	Result* evaluateVariable(const string&, const string&); // e.g. i(v, _, _)
+	Result* evaluateUnderscore(const string&); // e.g. i(_, _, _)
+	Result* evaluateFixed(const string&, const string&); // e.g. i("count", _, _)
+
+public:
+	// first arg represents the subject of clause (must be IF type), second arg represents control variable (left arg)
+	PatternIf(const DesignEntity&, const DesignEntity&);
+	Result evaluate(const PKB&) override;
+};
