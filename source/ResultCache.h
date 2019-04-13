7f11745e523477e83edbf08dbaa902bf3dfdbf38
sherlynng <sherlyn.xinler@gmail.com>
2019-04-13 17:22:48 +0800

    Remove namespace std, uncomment switch code, remove redundant code

--- source/ResultCache.h
+++ source/ResultCache.h
@@ -10,15 +10,15 @@
 
 class ResultCache {
 private:
-	unordered_map<CacheType, unordered_map<int, unordered_set<int>>> SynSynSame; // Affects(a, a)
-	unordered_map<CacheType, unordered_map<int, unordered_set<int>>> SynSynDiff; // Affects(a, a1)
-	unordered_map<Type, unordered_set<int>> SynUnderscore; // Affects(a, _)
-	unordered_map<Type, unordered_set<int>> UnderscoreSyn; // Affects(_, a)
-	unordered_map<Type, unordered_map<int, unordered_set<int>>> SynFixed; // Affects(a, 1)
-	unordered_map<Type, unordered_map<int, unordered_set<int>>> FixedSyn; // Affects(1, a)
+	std::unordered_map<CacheType, std::unordered_map<int, std::unordered_set<int>>> SynSynSame; // Affects(a, a)
+	std::unordered_map<CacheType, std::unordered_map<int, std::unordered_set<int>>> SynSynDiff; // Affects(a, a1)
+	std::unordered_map<Type, std::unordered_set<int>> SynUnderscore; // Affects(a, _)
+	std::unordered_map<Type, std::unordered_set<int>> UnderscoreSyn; // Affects(_, a)
+	std::unordered_map<Type, std::unordered_map<int, std::unordered_set<int>>> SynFixed; // Affects(a, 1)
+	std::unordered_map<Type, std::unordered_map<int, std::unordered_set<int>>> FixedSyn; // Affects(1, a)
 
-	unordered_map<int, unordered_set<int>> twoSynCacheResult;
-	unordered_set<int> oneSynCacheResult;
+	std::unordered_map<int, std::unordered_set<int>> twoSynCacheResult;
+	std::unordered_set<int> oneSynCacheResult;
 
 	bool isStmtType(Type type);
 	CacheType convertToCacheType(Type type1, Type type2);
@@ -36,23 +36,23 @@ private:
 public:
 	void resetCache(); // reset cache after each query
 	bool cacheExists(Clause* clause); // checks if cached result exists for the clause
-	unordered_set<int> getOneSynCacheResult(); // get cached result for single synonym (cached result is set after cacheExists() is called and is true)
-	unordered_map<int, unordered_set<int>> getTwoSynCacheResult(); // get cached result for double synonyms (cached result is set after cacheExists() is called and is true)
-	void storeInCache(Clause* clause, unordered_set<int>& result); // store result in cache for single synonym clause
-	void storeInCache(Clause* clause, unordered_map<int, unordered_set<int>>& result); // store result in cache for double synonyms clause
+	std::unordered_set<int> getOneSynCacheResult(); // get cached result for single synonym (cached result is set after cacheExists() is called and is true)
+	std::unordered_map<int, std::unordered_set<int>> getTwoSynCacheResult(); // get cached result for double synonyms (cached result is set after cacheExists() is called and is true)
+	void storeInCache(Clause* clause, std::unordered_set<int>& result); // store result in cache for single synonym clause
+	void storeInCache(Clause* clause, std::unordered_map<int, std::unordered_set<int>>& result); // store result in cache for double synonyms clause
 
 	// for testing (getter & setter)
 	bool isEquals(ResultCache other);
-	void ResultCache::setSynSynSame(unordered_map<CacheType, unordered_map<int, unordered_set<int>>> result);
-	void ResultCache::setSynSynDiff(unordered_map<CacheType, unordered_map<int, unordered_set<int>>> result);
-	void ResultCache::setSynUnderscore(unordered_map<Type, unordered_set<int>> result);
-	void ResultCache::setUnderscoreSyn(unordered_map<Type, unordered_set<int>> result);
-	void ResultCache::setSynFixed(unordered_map<Type, unordered_map<int, unordered_set<int>>> result);
-	void ResultCache::setFixedSyn(unordered_map<Type, unordered_map<int, unordered_set<int>>> result);
-	unordered_map<CacheType, unordered_map<int, unordered_set<int>>> getSynSynSame();
-	unordered_map<CacheType, unordered_map<int, unordered_set<int>>> getSynSynDiff();
-	unordered_map<Type, unordered_set<int>> getSynUnderscore();
-	unordered_map<Type, unordered_set<int>> getUnderscoreSyn();
-	unordered_map<Type, unordered_map<int, unordered_set<int>>> getSynFixed();
-	unordered_map<Type, unordered_map<int, unordered_set<int>>> getFixedSyn();
+	void ResultCache::setSynSynSame(std::unordered_map<CacheType, std::unordered_map<int, std::unordered_set<int>>> result);
+	void ResultCache::setSynSynDiff(std::unordered_map<CacheType, std::unordered_map<int, std::unordered_set<int>>> result);
+	void ResultCache::setSynUnderscore(std::unordered_map<Type, std::unordered_set<int>> result);
+	void ResultCache::setUnderscoreSyn(std::unordered_map<Type, std::unordered_set<int>> result);
+	void ResultCache::setSynFixed(std::unordered_map<Type, std::unordered_map<int, std::unordered_set<int>>> result);
+	void ResultCache::setFixedSyn(std::unordered_map<Type, std::unordered_map<int, std::unordered_set<int>>> result);
+	std::unordered_map<CacheType, std::unordered_map<int, std::unordered_set<int>>> getSynSynSame();
+	std::unordered_map<CacheType, std::unordered_map<int, std::unordered_set<int>>> getSynSynDiff();
+	std::unordered_map<Type, std::unordered_set<int>> getSynUnderscore();
+	std::unordered_map<Type, std::unordered_set<int>> getUnderscoreSyn();
+	std::unordered_map<Type, std::unordered_map<int, std::unordered_set<int>>> getSynFixed();
+	std::unordered_map<Type, std::unordered_map<int, std::unordered_set<int>>> getFixedSyn();
 };
